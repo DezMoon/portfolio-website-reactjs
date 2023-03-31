085396578145904f5bed079a6b55bbf2d9119248
DezMoon <moongachiku@gmail.com>
2023-03-31 17:08:47 +0200

    Added scroll icon to home.jsx

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home_social">
+            <a href="https://web.facebook.com/desmond.chikumba.moonga" className="home_social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-facebook-f"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/desmond-chiku-moonga" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/DezMoon" className="home_social-icon" target="_blank" rel="noreferrer">
+            <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
